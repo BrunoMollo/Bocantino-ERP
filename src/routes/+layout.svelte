5a9cd3d158a2972dbea6ecf4121f07e6437e138f
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-12-02 16:05:45 -0300

    init skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Bocantino</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail"></svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
