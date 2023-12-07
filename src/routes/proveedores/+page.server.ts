496708c2d7b337f33d109c563dff90a518257cc8
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-12-07 00:14:53 -0300

    movi una variable al back :)

--- src/routes/proveedores/+page.server.ts
+++ src/routes/proveedores/+page.server.ts
@@ -1,1 +1,24 @@
+import type { PageServerLoad } from './$types';
 
+export const load: PageServerLoad = () => {
+	const proveedores = [
+		{ nombre: 'Proveedor1', email: 'Proveedor1@gmail.com', materias: ['Papa', 'Zapallo'] },
+		{
+			nombre: 'Proveedor2',
+			email: 'Proveedor2@gmail.com',
+			materias: ['Zanahoria', 'Huevos', 'Harina']
+		},
+		{
+			nombre: 'Proveedor3',
+			email: 'Proveedor3@gmail.com',
+			materias: ['Papa', 'Zapallo', 'Huevos']
+		},
+		{
+			nombre: 'Proveedor4',
+			email: 'Proveedor4@gmail.com',
+			materias: ['Papas']
+		}
+	];
+
+	return { proveedores, saludos: 'hola' };
+};
