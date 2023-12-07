8835dbf61ab87eccbe4818ad1c0a2ea633a4f989
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-12-07 00:59:45 -0300

    mostrar Productos

--- src/routes/productos/+page.server.ts
+++ src/routes/productos/+page.server.ts
@@ -1,30 +1,30 @@
-import type { PageServerLoad } from "./$types";
-import { db } from "$lib";
+import type { PageServerLoad } from './$types';
+import { db } from '$lib';
 
-export const load: PageServerLoad = async ({ }) => {
-	const resultSet = await db.query.t_product.findMany(
-		{
-			with: {
-				r_ingredient_product: {
-					columns: {
-						amount: true
-					},
-					with: { ingredient: true }
-				}
+export const load: PageServerLoad = async ({}) => {
+	const resultSet = await db.query.t_product.findMany({
+		with: {
+			r_ingredient_product: {
+				columns: {
+					amount: true
+				},
+				with: { ingredient: true }
 			}
-		})
+		}
+	});
 
-	const list = resultSet.map(({ id, desc, r_ingredient_product }) => ({
-		id, desc,
+	const products = resultSet.map(({ id, desc, r_ingredient_product }) => ({
+		id,
+		desc,
 		ingredients: r_ingredient_product.map(({ amount, ingredient }) => ({
 			id: ingredient.id,
 			name: ingredient.name,
 			unit: ingredient.unit,
-			amount,
+			amount
 		}))
-	}))
+	}));
 
-	console.log(JSON.stringify(list))
+	console.log(JSON.stringify(products));
 
-	return { list }
-}
+	return { products };
+};
--- src/routes/productos/+page.svelte
+++ src/routes/productos/+page.svelte
@@ -1,2 +1,49 @@
-list
+<script lang="ts">
+	import { TreeView, TreeViewItem } from '@skeletonlabs/skeleton';
 
+	export let data;
+	const { products } = data;
+</script>
+
+<div
+	class="table-container w-11/12"
+	style="border-radius: 10px; box-shadow: 0 6px 12px rgba(0,0,0,0.8)"
+>
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Producto</th>
+				<th>Ingredientes</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each products as {id, desc, ingredients}}
+				<tr >
+					<td >{id}</td>
+					<td>{desc}</td>
+					<td>
+						<TreeView>
+							<TreeViewItem>
+								Receta
+								<svelte:fragment slot="children">
+									{#each ingredients as { name, amount, unit }}
+										<TreeViewItem class="grid justify-between">
+											{name} ({amount} {unit})
+										</TreeViewItem>
+									{/each}
+								</svelte:fragment>
+							</TreeViewItem>
+						</TreeView>
+					</td>
+					<td class="grid">
+						<a class="btn-icon btn-icon-md variant-soft-secondary" href="/productos/edit/{id}">
+							<i class="bx bx-edit place-self-center text-2xl"></i>
+						</a>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
