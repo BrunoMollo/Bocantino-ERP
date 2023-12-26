64bd8100ef898c44627ed32ac61738de1d94d970
Filippo Bianconi <filippob2002@gmail.com>
2023-12-26 17:44:01 -0300

    Pre changes

--- src/routes/insumos-ingresados/+page.svelte
+++ src/routes/insumos-ingresados/+page.svelte
@@ -10,24 +10,10 @@
 	} from '@skeletonlabs/skeleton';
 	import { Paginator } from '@skeletonlabs/skeleton';
 
-	const source = [
-		{ position: 1, proveedor: 'Juan S.A', fechaAlta: '11-04-2023', codigoFactura: '32084fc08' },
-		{ position: 2, proveedor: 'Fernando S.A', fechaAlta: '11-04-2023', codigoFactura: '249fj24' },
-		{ position: 3, proveedor: 'Zapallo S.A', fechaAlta: '11-04-2023', codigoFactura: '428983rd' },
-		{ position: 4, proveedor: 'Beryllium S.A', fechaAlta: '11-04-2023', codigoFactura: '49fjw942' },
-		{ position: 5, proveedor: 'Corral S.A.', fechaAlta: '11-04-2023', codigoFactura: '5f8j39f' }
-	];
-
-	const tableSimple: TableSource = {
-		head: ['ID', 'Proveedor', 'Fecha alta', 'Codigo Factura'],
-		body: tableMapperValues(source, ['position', 'proveedor', 'fechaAlta', 'codigoFactura']),
-		meta: tableMapperValues(source, ['position', 'Proveedor', 'FechaAlta', 'codigoFactura'])
-	};
-
 	let paginationSettings = {
 		page: 0,
 		limit: 10,
-		size: source.length,
+		size: 100,
 		amounts: [1, 2, 5, 10]
 	} satisfies PaginationSettings;
 
@@ -54,7 +40,18 @@
 	<div class="arrow variant-filled-primary" />
 </div>
 <div class="w-11/12 mx-auto mt-10">
-	<Table source={tableSimple} interactive={true} class="mb-4" />
+	<table class="table mb-4 rounded">
+		<thead>
+			<tr>
+				<th class="text-center w-2/12">ID:</th>
+				<th class="text-center w2/12">Proveedor:</th>
+				<th class="text-center w-2/12">Fecha alta:</th>
+				<th class="text-center w-2/12">Codigo factura:</th>
+				<th class="text-center w-2/12"></th>
+			</tr>
+		</thead>
+		<tbody> </tbody>
+	</table>
 	<Paginator
 		bind:settings={paginationSettings}
 		showFirstLastButtons={true}
