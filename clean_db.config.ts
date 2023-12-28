e6ed8d38b963881b23f69b0fceb2b7617c69ba33
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-12-27 23:11:40 -0300

    comand to delete all testing db

---
+++ clean_db.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const { TURSO_URL, TURSO_TOKEN } = process.env;
+if (!TURSO_URL || !TURSO_TOKEN) {
+	throw new Error('Define TURSO_URL and TURSO_TOKEN in .env file');
+}
+
+export default {
+	schema: './clean_db.config.ts',
+	driver: 'turso',
+	dbCredentials: {
+		url: TURSO_URL,
+		authToken: TURSO_TOKEN
+	}
+} satisfies Config;
+
