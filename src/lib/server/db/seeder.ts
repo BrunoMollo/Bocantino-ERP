8f53b9995fc37253070c0fe3c9895f934b9d24ae
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-12-30 09:22:42 -0300

    modifier seeder

--- src/lib/server/db/seeder.ts
+++ src/lib/server/db/seeder.ts
@@ -1,8 +1,6 @@
-// modules are imported with required because is was the easyest way to run the script,
-// if you change it it might brake the `npm run db-clean` comand.
-const { createClient } = require('@libsql/client');
-const { drizzle } = require('drizzle-orm/libsql');
-const {
+import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
+import {
 	t_document_type,
 	t_entry_document,
 	t_ingredient,
@@ -13,9 +11,9 @@ const {
 	tr_ingredient_ingredient,
 	tr_ingredient_product,
 	tr_supplier_ingredient
-} = require('./schema');
-const schema = require('./schema');
-const dotenv = require('dotenv');
+} from './schema';
+import * as schema from './schema';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 const { TURSO_URL, TURSO_TOKEN } = process.env;
@@ -52,9 +50,59 @@ async function seed() {
 	await Promise.all([
 		db.insert(t_document_type).values({ desc: 'Factura' }),
 		db.insert(t_document_type).values({ desc: 'Remito' }),
-		db.insert(t_document_type).values({ desc: 'Orden de compra' }),
-		db.insert(t_supplier).values({ name: 'BOCANTINO', email: 'not-needed' })
+		db.insert(t_document_type).values({ desc: 'Orden de compra' })
 	]);
+	await db
+		.insert(t_ingredient)
+		.values({ id: 1, name: 'Higado', unit: 'Kilogramos', reorderPoint: 100 });
+
+	await db
+		.insert(t_ingredient)
+		.values({ id: 2, name: 'Higado  desidratado', unit: 'Kilogramos', reorderPoint: 200 });
+	await db.insert(tr_ingredient_ingredient).values({ derivedId: 2, sourceId: 1, amount: 2 });
+
+	await db.insert(t_supplier).values({ id: 1, name: 'julian', email: 'julian@hotmail.com' });
+	await db.insert(tr_supplier_ingredient).values({ supplierId: 1, ingredientId: 1 });
+
+	await db.insert(t_ingredient_batch).values({
+		id: 1,
+		batch_code: 'ABCEDE_1234',
+		initialAmount: 300,
+		productionDate: new Date(2023, 12, 12),
+		expirationDate: new Date(2023, 1, 30),
+		ingredientId: 1,
+		numberOfBags: 10,
+		state: 'AVAILABLE',
+		supplierId: 1,
+		cost: 4000
+	});
+
+	await db.insert(t_ingredient_batch).values({
+		id: 2,
+		batch_code: 'XYZP_1234',
+		initialAmount: 200,
+		productionDate: new Date(2023, 12, 30),
+		expirationDate: new Date(2023, 2, 30),
+		ingredientId: 1,
+		numberOfBags: 10,
+		state: 'AVAILABLE',
+		supplierId: 1,
+		cost: 4500
+	});
+
+	await db.insert(t_ingredient_batch).values({
+		id: 3,
+		batch_code: 'PPPP_1234',
+		initialAmount: 200,
+		usedAmount: 200,
+		productionDate: new Date(2023, 12, 30),
+		expirationDate: new Date(2023, 2, 30),
+		ingredientId: 1,
+		numberOfBags: 10,
+		state: 'EMPTY',
+		supplierId: 1,
+		cost: 2000
+	});
 }
 
 async function main() {
