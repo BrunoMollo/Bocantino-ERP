60e13cef9ac8b0edc6670f18c63c83c617d8bdb4
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-01-10 02:15:46 -0300

    redirect / to /bocantino in hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,9 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	if (event.url.pathname === '/') {
+		throw redirect(302, '/bocantino');
+	}
 	const respose = await resolve(event);
 	return respose;
 };
