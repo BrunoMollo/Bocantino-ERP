917737ad611e1810cd914a15209b2656bba9cd4c
Filippo Bianconi <filippob2002@gmail.com>
2024-01-12 02:36:19 -0300

    login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,146 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<link rel="preconnect" href="https://fonts.gstatic.com" />
-		<link
-			rel="stylesheet"
-			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-		/>
-		<link
-			href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap"
-			rel="stylesheet"
-		/>
-		<style media="screen">
-			*,
-			*:before,
-			*:after {
-				padding: 0;
-				margin: 0;
-				box-sizing: border-box;
-			}
-			body {
-				background-color: #080710;
-			}
-			.background {
-				width: 430px;
-				height: 520px;
-				position: absolute;
-				transform: translate(-50%, -50%);
-				left: 50%;
-				top: 50%;
-			}
-			.background .shape {
-				height: 200px;
-				width: 200px;
-				position: absolute;
-				border-radius: 50%;
-			}
-			.shape:first-child {
-				background: linear-gradient(#1845ad, #23a2f6);
-				left: -80px;
-				top: -80px;
-			}
-			.shape:last-child {
-				background: linear-gradient(to right, #ff512f, #f09819);
-				right: -30px;
-				bottom: -80px;
-			}
-			form {
-				height: 520px;
-				width: 400px;
-				background-color: rgba(255, 255, 255, 0.13);
-				position: absolute;
-				transform: translate(-50%, -50%);
-				top: 50%;
-				left: 50%;
-				border-radius: 10px;
-				backdrop-filter: blur(10px);
-				border: 2px solid rgba(255, 255, 255, 0.1);
-				box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
-				padding: 50px 35px;
-			}
-			form * {
-				font-family: 'Poppins', sans-serif;
-				color: #ffffff;
-				letter-spacing: 0.5px;
-				outline: none;
-				border: none;
-			}
-			form h3 {
-				font-size: 32px;
-				font-weight: 500;
-				line-height: 42px;
-				text-align: center;
-			}
-
-			label {
-				display: block;
-				margin-top: 30px;
-				font-size: 16px;
-				font-weight: 500;
-			}
-			input {
-				display: block;
-				height: 50px;
-				width: 100%;
-				background-color: rgba(255, 255, 255, 0.07);
-				border-radius: 3px;
-				padding: 0 10px;
-				margin-top: 8px;
-				font-size: 14px;
-				font-weight: 300;
-			}
-			::placeholder {
-				color: #e5e5e5;
-			}
-			button {
-				margin-top: 50px;
-				width: 100%;
-				background-color: #ffffff;
-				color: #080710;
-				padding: 15px 0;
-				font-size: 18px;
-				font-weight: 600;
-				border-radius: 5px;
-				cursor: pointer;
-			}
-			button:hover {
-				background-color: rgba(255, 255, 255, 0.8);
-			}
-			.social {
-				margin-top: 30px;
-			}
-			.social div {
-				border-radius: 3px;
-				padding: 5px 10px 10px 5px;
-				background-color: rgba(255, 255, 255, 0.27);
-				color: #eaf0fb;
-				text-align: center;
-			}
-			.social div:hover {
-				background-color: rgba(255, 255, 255, 0.47);
-				cursor: pointer;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="background">
-			<div class="shape"></div>
-			<div class="shape"></div>
-		</div>
-		<form class="">
-			<h3>BOCANTINO LOGIN</h3>
-
-			<label for="username">Username</label>
-			<input type="text" placeholder="Email or Phone" id="username" />
-
-			<label for="password">Password</label>
-			<input type="password" placeholder="Password" id="password" />
-
-			<button>Log In</button>
-			<div class="social w-full">
-				<div class="">Forgot password...</div>
+<div class="w-full h-full flex items-center">
+	<div class="card w-6/12 h-2/4 m-auto pt-10 rounded-lg p-3 shadow-2xl">
+		<h1 class="text-5xl uppercase text-center">Bocantino Login</h1>
+		<form action="" class="form">
+			<div class="w-2/3 mx-auto mt-10">
+				<p>Usuario:</p>
+				<input type="text" class="input w-full" />
+			</div>
+			<div class="w-2/3 mx-auto mt-5">
+				<p>Password:</p>
+				<input type="text" class="input w-full" />
+			</div>
+			<div class="w-2/3 mx-auto flex justify-between mt-5">
+				<button>Olvide mi contrasena</button>
+				<button class="btn variant-filled w-32 rounded">Iniciar sesion</button>
 			</div>
 		</form>
-	</body>
-</html>
+	</div>
+</div>
