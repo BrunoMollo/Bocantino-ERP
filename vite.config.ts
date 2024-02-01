c9df5b140e404fd6b2bcf21414c579fbadcfdcd3
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-02-01 01:16:52 -0300

    change port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	server: {
+		port: 8000,
+		strictPort: false
+	},
+	preview: {
+		port: 4173,
+		strictPort: false
+	}
 });
 
