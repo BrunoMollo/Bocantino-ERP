18a3a1812754cf20a71441931c6184c2120a2380
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-02-02 14:31:25 -0300

    remove console.log

--- src/routes/bocantino/materias-primas/_components/shared.ts
+++ src/routes/bocantino/materias-primas/_components/shared.ts
@@ -22,7 +22,6 @@ export type IngredientSchema = typeof ingredient_schema;
 
 export function createForm(value?: typeof ingredient_schema._type) {
 	if (value) {
-		console.log(value);
 		return superValidate(value, ingredient_schema, { errors: false });
 	} else {
 		return superValidate(ingredient_schema);
