49a4dad7c2e8e63f176d37d119ffcc7b5fef4f6b
BianconiFilippo <filippob2002@gmail.com>
2024-02-07 23:45:37 -0300

    First step refactor

--- src/routes/bocantino/+page.server.ts
+++ src/routes/bocantino/+page.server.ts
@@ -1,8 +1,12 @@
+import { purchases_service } from '$logic/ingredient-purchase-service';
 import { ingredients_service } from '$logic/ingredient-service';
+import { product_service } from '$logic/product-service';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const ingredients = ingredients_service.getAllWithStock();
-	return { ingredients };
+	const pending_productions = await product_service.getLastBatchesInProduction();
+	const entries = purchases_service.getLastEntries();
+	return { ingredients, entries, pending_productions };
 };
 
