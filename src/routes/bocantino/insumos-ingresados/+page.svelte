948a2123922d9f869e7ec3df2d30554edd1b0c8a
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-02-10 16:36:06 -0300

    fix UI insumos-ingresados

--- src/routes/bocantino/insumos-ingresados/+page.svelte
+++ src/routes/bocantino/insumos-ingresados/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { trpc } from '$lib/trpc-client';
-	import Loader from '../_components/Loader.svelte';
-	import Detalle from './_components/Detalle.svelte';
 	import { popup, type PaginationSettings, type PopupSettings } from '@skeletonlabs/skeleton';
 	import { Paginator } from '@skeletonlabs/skeleton';
 	export let data;
@@ -25,15 +23,6 @@
 		number: ''
 	};
 	let listafiltrada = data.entries;
-
-	let selected_entry: (typeof data.entries)[0] | undefined = undefined;
-
-	function expandirDetalle(idEntrada: string) {
-		if (idEntrada) {
-			selected_entry = data.entries.find((x) => x.id == Number(idEntrada));
-			return null;
-		}
-	}
 
 	async function filtrar() {
 		listafiltrada = await trpc.entries.get
@@ -124,13 +113,12 @@
 						>{entrada.document.number} ({entrada.document.type})</td
 					>
 					<td style="vertical-align:middle" class="text-center w-2/12">
-						<button
-							type="button"
-							class="btn variant-filled-primary rounded"
-							on:click={expandirDetalle(entrada.id.toString())}>Detalles</button
-						></td
-					>
-				</tr>
+						<a
+							class="btn variant-filled-secondary"
+							href={'/bocantino/insumos-ingresados/' + entrada.id}>ver detalle</a
+						>
+					</td></tr
+				>
 			{/each}
 		</tbody>
 	</table>
@@ -143,4 +131,3 @@
 	/>
 </div>
 
-<Detalle {selected_entry} />
