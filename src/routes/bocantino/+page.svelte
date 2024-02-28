c481585269aa85b9b87d63225443ba87c3dbe763
BianconiFilippo <filippob2002@gmail.com>
2024-02-28 16:37:42 -0300

    ahora si

--- src/routes/bocantino/+page.svelte
+++ src/routes/bocantino/+page.svelte
@@ -114,33 +114,5 @@
 				<p>Stock real: {Math.round(stock * 10) / 10}</p>
 			</div>
 		{/each}
-		{#each ingredientes as { id, name, stock, reorder_point }}
-			<div
-				class="shrink-0 card p-3 w-96 rounded-lg shadow-lg relative"
-				style:background-color={stock < reorder_point ? 'rgba(127, 29, 29, 0.4)' : ''}
-				style:box-shadow={stock < reorder_point ? '0 1px 25px 1px rgba(255, 0, 0, 0.8)' : ''}
-			>
-				{#if stock < reorder_point}
-					<div class="absolute right-5 bottom-4">
-						<ProgressRadial
-							width="w-24"
-							font={128}
-							stroke={70}
-							value={(stock / reorder_point) * 100}
-						>
-							{Math.round((stock / reorder_point) * 100)}%
-						</ProgressRadial>
-					</div>
-				{:else}
-					<div class="absolute right-5 bottom-4">
-						<ProgressRadial width="w-24" font={128} stroke={70} value={100}>OK</ProgressRadial>
-					</div>
-				{/if}
-				<h1>ID:{id}</h1>
-				<h2 class="uppercase text-xl w-9/12">{name}</h2>
-				<p class="mt-3">Punto de pedido: {reorder_point}</p>
-				<p>Stock real: {Math.round(stock * 10) / 10}</p>
-			</div>
-		{/each}
 	</div>
 </main>
