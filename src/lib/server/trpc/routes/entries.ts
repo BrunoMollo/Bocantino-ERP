cee4fb60604e3987006f58045bf88bde29b638f1
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-03-02 20:52:41 -0300

    removed unused trpc function

--- src/lib/server/trpc/routes/entries.ts
+++ src/lib/server/trpc/routes/entries.ts
@@ -3,27 +3,6 @@ import { publicProcedure, router } from '../context';
 import { purchases_service } from '$logic/ingredient-purchase-service';
 
 export const entries = router({
-	get: publicProcedure
-		.input(
-			z.object({
-				supplierName: z.string().nullable(),
-				documentNumber: z.string(),
-				page: z.number().int(),
-				pageSize: z.number().int().max(20),
-				dateInitial: z.date().nullable(),
-				dateFinal: z.date().nullable()
-			})
-		)
-		.query(async ({ input }) => {
-			return await purchases_service.getEntries({
-				supplierName: input.supplierName ?? '',
-				dateInitial: input.dateInitial ?? undefined,
-				dateFinal: input.dateFinal ?? undefined,
-				documentNumber: input.documentNumber ?? '',
-				page: input.page,
-				pageSize: input.pageSize
-			});
-		}),
 	getBatches: publicProcedure.input(z.number().int().positive()).query(async ({ input }) => {
 		return await purchases_service.getBatchesByEntryId(input);
 	})
