67908a8d2eabc018edaa1116c5f2f8c1133274be
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-03-03 19:30:57 -0300

    move most of page

---
+++ src/routes/bocantino/solicitudes-pendientes/ingredientes/[id]/+page.server.ts
@@ -0,0 +1,62 @@
+import { superValidate } from 'sveltekit-superforms/client';
+import { z } from 'zod';
+import type { PageServerLoad, RouteParams } from './$types';
+import { error, type Actions } from '@sveltejs/kit';
+import { ingredient_production_service } from '$logic/ingredient-production-service';
+
+const close_production_schema = z.object({
+	batch_id: z.coerce.number().int().positive(),
+	adjustment: z.number()
+});
+
+const cancel_production_schema = z.object({
+	batch_id: z.coerce.number().int().positive()
+});
+
+export const load: PageServerLoad = async ({ params }) => {
+	const { id } = parse_id_param(params);
+	const batch = await ingredient_production_service.getBatchById(id);
+	if (!batch) {
+		throw error(404, 'lote no existe');
+	}
+	const form = superValidate(close_production_schema);
+	const cancel_form = superValidate(cancel_production_schema);
+	return { batch, form, cancel_form };
+};
+
+function parse_id_param(params: RouteParams) {
+	const id = Number(params.id);
+	if (isNaN(id) || id < 0) {
+		throw error(400, { message: 'invalid id' });
+	}
+	return { id };
+}
+
+export const actions: Actions = {
+	finish: async ({ request }) => {
+		const form = await superValidate(request, close_production_schema);
+		if (!form.valid) {
+			return { form };
+		}
+		const res = await ingredient_production_service.closeProduction(form.data);
+		if (res.type == 'LOGIC_ERROR') {
+			throw error(400, res.message);
+		}
+		return { form };
+	},
+
+	cancel: async ({ request }) => {
+		const form = await superValidate(request, cancel_production_schema);
+		if (!form.valid) {
+			return { form };
+		}
+		const { batch_id } = form.data;
+		const res = await ingredient_production_service.deleteBatchById(batch_id);
+
+		if (res.type == 'LOGIC_ERROR') {
+			throw error(400, res.message);
+		}
+
+		return { form };
+	}
+};
