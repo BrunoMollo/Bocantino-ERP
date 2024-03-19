eecdaca31d8aacc5e78f8608685c2c898eb8fac4
Bruno Mollo <48107910+BrunoMollo@users.noreply.github.com>
2024-03-19 16:50:49 -0300

    77 add calories to nutrients (#82)
    * calculate calories
    
    * separated nutrients utils to different file & fix imports
    
    * fix unit tests
    
    * [Github Action]->format
    
    * rename function
    
    * rename function unit_nutrietns
    
    ---------
    
    Co-authored-by: BrunoMollo <BrunoMollo@users.noreply.github.com>
    Co-authored-by: Bianconi Filippo <96350624+BianconiFilippo@users.noreply.github.com>

--- src/routes/bocantino/materias-primas/_components/MateriaPrimaForm.svelte
+++ src/routes/bocantino/materias-primas/_components/MateriaPrimaForm.svelte
@@ -3,7 +3,8 @@
 	import { fade } from 'svelte/transition';
 	import { superForm } from 'sveltekit-superforms/client';
 	import { VALID_UNITS, type IngredientSchema } from './shared';
-	import { name_nutrient, startAs } from '$lib/utils';
+	import { startAs } from '$lib/utils';
+	import { nutrient_name } from '$lib/nutrients-utils';
 
 	export let ingredients: { id: number; name: string; unit: string }[];
 
@@ -144,7 +145,7 @@
 		{#each nutrients as n}
 			<div>
 				<label class="label" transition:fade for={n}>
-					<span>{name_nutrient(n)}</span>
+					<span>{nutrient_name(n)}</span>
 					{#if $errors[n]}
 						<b class=" text-error-400" transition:fade>Ingrese un valor valido</b>
 					{/if}
