b63f37a60a3fc5f5d1c10a695aeed792a4c3be0e
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-04-27 16:46:17 -0300

    modify reddirect page

--- src/routes/bocantino/ingreso-insumos/+page.svelte
+++ src/routes/bocantino/ingreso-insumos/+page.svelte
@@ -3,26 +3,30 @@
 
 	export const routes = [
 		{
-			name: 'Ingreso por Factura',
+			name: 'Ingreso por Factura >>',
 			href: '/bocantino/ingreso-insumos/factura'
 		},
 		{
-			name: 'Ingreso por Remito',
-			href: '/bocantino/ingreso-insumos/remito'
+			name: 'Ingreso por Nota de Ingreso >>',
+			href: '/bocantino/ingreso-insumos/nota-de-ingreso'
 		},
 		{
-			name: 'Ingreso por Nota de Ingreso',
-			href: '/bocantino/ingreso-insumos/nota-de-ingreso'
+			name: 'Ingreso por Remito [Pendiente] >>',
+			href: '/bocantino/ingreso-insumos/remito'
 		}
 	] satisfies { name: string; href: LayoutRouteId }[];
 </script>
 
 <main class="container h-full mx-auto flex justify-center items-center">
-	<ul>
-		{#each routes as { name, href }}
-			<li class="pt-6">
-				<a {href}>{name}</a>
-			</li>
-		{/each}
-	</ul>
+	<div>
+		<h2 class="h2">Ingreso de insumos</h2>
+		<p>Seleccione el tipo de ingreso correpondiente</p>
+		<ul>
+			{#each routes as { name, href }}
+				<li class="pt-6">
+					<a class="btn variant-filled" {href}>{name}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </main>
