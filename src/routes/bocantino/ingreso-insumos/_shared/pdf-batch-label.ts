ee9ba23a161bcf87d37d34e32249879e34ea1c4a
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-05-07 19:32:41 -0300

    format date and refactor

--- src/routes/bocantino/ingreso-insumos/_shared/pdf-batch-label.ts
+++ src/routes/bocantino/ingreso-insumos/_shared/pdf-batch-label.ts
@@ -1,7 +1,7 @@
 import { jsPDF } from 'jspdf';
 
 export function printBatchLabel(
-	information: {
+	batch: {
 		batch_code: string;
 		initial_amount: number;
 		production_date: Date;
@@ -15,24 +15,33 @@ export function printBatchLabel(
 		name: string;
 	}>
 ) {
-	const fechaHoy = new Date();
+	const this_year = new Date().getFullYear();
 	const doc = new jsPDF({
 		orientation: 'landscape',
 		unit: 'mm',
 		format: [80, 50]
 	});
+
+	const ingredient_name = ingredients.find((x) => x.id === batch.ingredient_id)?.name;
+	const production_date = format_date(batch.production_date);
+	const expiration_date = format_date(batch.expiration_date);
+	const { batch_code, initial_amount, number_of_bags } = batch;
+
 	doc.setFontSize(8);
-	const ingredient_name = ingredients.find((x) => x.id === information.ingredient_id)?.name;
 	doc.text('Materia prima: ' + ingredient_name ?? '[[ERROR: Ingrediente no encontrado]]', 5, 5);
 	doc.text('Fechas:', 5, 11);
-	doc.text('Produccion: ' + information.production_date.toLocaleDateString('es'), 25, 11);
-	doc.text('Vencimiento: ' + information.expiration_date.toLocaleDateString('es'), 25, 15);
+	doc.text('Produccion: ' + production_date, 25, 11);
+	doc.text('Vencimiento: ' + expiration_date, 25, 15);
 	doc.setFontSize(13);
-	doc.text('Codigo Lote ' + information.batch_code, 3, 30);
+	doc.text('Codigo Lote ' + batch_code, 3, 30);
 	doc.setFontSize(8);
-	doc.text('Cantidad: ' + information.initial_amount / information.number_of_bags + 'Kg', 5, 37);
+	doc.text('Cantidad: ' + initial_amount / number_of_bags + 'Kg', 5, 37);
 	doc.line(5, 40, 75, 40);
-	doc.text('©' + fechaHoy.getFullYear() + 'BOCANTINO. Todos los derechos reservados.', 5, 45);
+	doc.text('© ' + this_year + ' BOCANTINO. Todos los derechos reservados.', 5, 45);
 	doc.autoPrint({ variant: 'non-conform' });
-	doc.save('Etiqueta' + information.batch_code);
+	doc.save('Etiqueta' + batch_code);
+}
+
+function format_date(date: Date | string) {
+	return new Date(date).toLocaleDateString('es');
 }
