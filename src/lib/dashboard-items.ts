65b7737eecfc6cf92dc5243fcfc1e3a34b341d47
BrunoMollo <BrunoMollo@users.noreply.github.com>
2025-07-21 22:47:01 +0000

    [Github Action]->format

--- src/lib/dashboard-items.ts
+++ src/lib/dashboard-items.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 export const items = writable([
-    { name: 'Producciones pendientes', show: true },
-    { name: 'Últimos ingresos', show: true }
+	{ name: 'Producciones pendientes', show: true },
+	{ name: 'Últimos ingresos', show: true }
 ]);
