8e4dad983598167a3f66d838202cefa08f28a76b
BrunoMollo <dia.mollo.bruno@gmail.com>
2025-08-31 20:58:48 -0300

    restore files that caused conflicts and broke auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,37 +1,37 @@
-// import { auth_service } from '$logic/auth-service';
-// import type { Handle } from '@sveltejs/kit';
+import { auth_service } from '$logic/auth-service';
+import type { Handle } from '@sveltejs/kit';
 
-// export const handle: Handle = async ({ event, resolve }) => {
-// 	if (event.url.pathname === '/login' || event.url.pathname == '/-dev') {
-// 		return await resolve(event);
-// 	}
+export const handle: Handle = async ({ event, resolve }) => {
+	if (event.url.pathname === '/login' || event.url.pathname == '/-dev') {
+		return await resolve(event);
+	}
 
-// 	const token = event.cookies.get('token');
+	const token = event.cookies.get('token');
 
-// 	if (!token) {
-// 		return hook_redirect('/login');
-// 	}
+	if (!token) {
+		return hook_redirect('/login');
+	}
 
-// 	const res = await auth_service.verifyJWT(token);
+	const res = await auth_service.verifyJWT(token);
 
-// 	if (res.type == 'AUTH_ERROR') {
-// 		return hook_redirect('/login');
-// 	}
+	if (res.type == 'AUTH_ERROR') {
+		return hook_redirect('/login');
+	}
 
-// 	event.locals.user = res.payload;
+	event.locals.user = res.payload;
 
-// 	if (event.url.pathname === '/') {
-// 		return hook_redirect('/bocantino');
-// 	}
+	if (event.url.pathname === '/') {
+		return hook_redirect('/bocantino');
+	}
 
-// 	const respose = await resolve(event);
-// 	return respose;
-// };
+	const respose = await resolve(event);
+	return respose;
+};
 
-// /* custom redirect */
-// function hook_redirect(location: string) {
-// 	return new Response(undefined, {
-// 		status: 303,
-// 		headers: { location }
-// 	});
-// }
+/* custom redirect */
+function hook_redirect(location: string) {
+	return new Response(undefined, {
+		status: 303,
+		headers: { location }
+	});
+}
